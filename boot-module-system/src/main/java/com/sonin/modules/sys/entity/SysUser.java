09ca43c37b10ea74fbcf2afd9f22e889b85f5b55
sonin <1457065857@qq.com>
2022-03-27 18:20:03 +0800

    优化中

--- boot-module-system/src/main/java/com/sonin/modules/sys/entity/SysUser.java
+++ boot-module-system/src/main/java/com/sonin/modules/sys/entity/SysUser.java
@@ -1,12 +1,9 @@
 package com.sonin.modules.sys.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
