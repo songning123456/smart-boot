8742bc905f0e93450e9a45a4191a73ccae955e7b
sonin <1457065857@qq.com>
2022-06-19 14:42:47 +0800

    二分搜索

---
+++ boot-module-base/src/main/java/com/sonin/modules/labuladong/service/shuzu/erfensousuo/SearchRange.java
@@ -0,0 +1,59 @@
+package com.sonin.modules.labuladong.service.shuzu.erfensousuo;
+
+import com.sonin.modules.labuladong.service.Solution;
+
+/**
+ * <pre>
+ * <请输入描述信息>
+ * </pre>
+ *
+ * @author sonin
+ * @version 1.0 2022/6/19 14:18
+ */
+public class SearchRange implements Solution {
+
+    @Override
+    public Object handle() {
+        return null;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{leftBound(nums, target), rightBound(nums, target)};
+    }
+
+    int leftBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                right = mid - 1;
+            }
+        }
+        if (left >= nums.length || nums[left] != target) {
+            return -1;
+        }
+        return left;
+    }
+
+    int rightBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                left = mid + 1;
+            }
+        }
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+        return right;
+    }
+}
