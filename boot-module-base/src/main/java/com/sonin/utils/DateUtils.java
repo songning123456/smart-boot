074596fce2966cae2002f259f654863138f9f35f
sonin <1457065857@qq.com>
2022-07-07 14:07:38 +0800

    DateUtils补充 某周/某月天数 功能

--- boot-module-base/src/main/java/com/sonin/utils/DateUtils.java
+++ boot-module-base/src/main/java/com/sonin/utils/DateUtils.java
@@ -2,6 +2,9 @@ package com.sonin.utils;
 
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 /**
@@ -207,6 +210,90 @@ public class DateUtils {
      */
     public static Date nextYear(Date currentDate) {
         return someDate(currentDate, Calendar.YEAR, 1);
+    }
+
+    /**
+     * <pre>
+     * 获取某一周: 0:这周; -1:上一周; 1:下一周;
+     * </pre>
+     *
+     * @param
+     * @author sonin
+     * @Description: TODO(这里描述这个方法的需求变更情况)
+     */
+    public static List<String> someWeek(int n) {
+        String format = "yyyy-MM-dd";
+        // 获取本周
+        DayOfWeek dayOfWeek = LocalDateTime.now().getDayOfWeek();
+        // 本周周一
+        String startTime = LocalDateTime.now().minusDays(7 - dayOfWeek.getValue()).format(DateTimeFormatter.ofPattern(format));
+        // 本周周日
+        String endTime = LocalDateTime.now().plusDays(7 - dayOfWeek.getValue()).format(DateTimeFormatter.ofPattern(format));
+        if (n != 0) {
+            startTime = date2Str(someDate(strToDate(startTime, format), Calendar.DATE, n * 7), format);
+            endTime = date2Str(someDate(strToDate(endTime, format), Calendar.DATE, n * 7), format);
+        }
+        return intervalByDay(startTime, endTime, format);
+    }
+
+    /**
+     * <pre>
+     * 获取某一个月: 0:这个月; -1:上一月; 1:下一月;
+     * </pre>
+     *
+     * @param
+     * @author sonin
+     * @Description: TODO(这里描述这个方法的需求变更情况)
+     */
+    public static List<String> someMonth(int n) {
+        Date date = someDate(new Date(), Calendar.MONTH, n);
+        Calendar calendar = Calendar.getInstance(Locale.CHINA);
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR); //年份
+        int month = calendar.get(Calendar.MONTH) + 1; //月份
+        int day = calendar.getActualMaximum(Calendar.DATE); // 天数
+        String startTime = year + "-" + (month < 10 ? ("0" + month) : month) + "-01";
+        String endTime = year + "-" + (month < 10 ? ("0" + month) : month) + "-" + day;
+        return intervalByDay(startTime, endTime, "yyyy-MM-dd");
+    }
+
+    /**
+     * String => java.util.Date
+     *
+     * @param str        表示日期的字符串
+     * @param dateFormat 传入字符串的日期表示格式（如："yyyy-MM-dd HH:mm:ss"）
+     * @return java.util.Date类型日期对象（如果转换失败则返回null）
+     */
+    public static Date strToDate(String str, String dateFormat) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        Date date = null;
+        try {
+            date = simpleDateFormat.parse(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    /**
+     * <pre>
+     *  java.util.Date => String
+     * </pre>
+     *
+     * @param date
+     * @param format
+     * @author sonin
+     * @Description: TODO(这里描述这个方法的需求变更情况)
+     */
+    public static String date2Str(Date date, String format) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+        return simpleDateFormat.format(date);
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(someWeek(-1));
+        System.out.println(someMonth(1));
+        System.out.println("");
     }
 
 }
