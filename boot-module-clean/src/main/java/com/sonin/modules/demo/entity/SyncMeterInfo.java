12fb8799edb804801dae386d434199e4a7d62f64
sonin <1457065857@qq.com>
2022-09-20 16:46:17 +0800

    demo

---
+++ boot-module-clean/src/main/java/com/sonin/modules/demo/entity/SyncMeterInfo.java
@@ -0,0 +1,24 @@
+package com.sonin.modules.demo.entity;
+
+import lombok.Data;
+
+/**
+ * <pre>
+ * <请输入描述信息>
+ * </pre>
+ *
+ * @author sonin
+ * @version 1.0 2022/9/20 15:09
+ */
+@Data
+public class SyncMeterInfo {
+
+    private String meterCode;
+
+    private String locType;
+
+    private String locClassify;
+
+    private String meterCategory;
+
+}
