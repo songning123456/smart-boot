9de9ae5778df6e2651ecfed96b2a43bf7ab5ef5d
sonin <1457065857@qq.com>
2023-01-05 16:19:34 +0800

    添加反射setObj

---
+++ boot-module-base/src/main/java/com/sonin/utils/ReflectUtils.java
@@ -0,0 +1,89 @@
+package com.sonin.utils;
+
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * <pre>
+ * <请输入描述信息>
+ * </pre>
+ *
+ * @author sonin
+ * @version 1.0 2022/11/14 15:19
+ */
+public class ReflectUtils {
+
+    /**
+     * <pre>
+     * 获取某一个类的所有字段名称
+     * </pre>
+     *
+     * @param clazz
+     * @author sonin
+     * @Description: TODO(这里描述这个方法的需求变更情况)
+     */
+    public static Map<String, Class> getAllField(Class clazz) {
+        Map<String, Class> fieldMap = new LinkedHashMap<>();
+        try {
+            Field[] fields;
+            while (!"java.lang.Object".equals(clazz.getName())) {
+                fields = clazz.getDeclaredFields();
+                for (Field field : fields) {
+                    fieldMap.put(field.getName(), field.getType());
+                }
+                clazz = clazz.getSuperclass();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fieldMap;
+    }
+
+    /**
+     * <pre>
+     * 设置对象
+     * </pre>
+     *
+     * @param entity
+     * @param paramMap
+     * @author sonin
+     * @Description: TODO(这里描述这个方法的需求变更情况)
+     */
+    public static void setObj(Object entity, Map<String, String> paramMap) throws Exception {
+        String fieldName, fieldValue, typeName;
+        for (Map.Entry<String, String> item : paramMap.entrySet()) {
+            fieldName = item.getKey();
+            fieldValue = item.getValue();
+            Class clazz = entity.getClass();
+            while (!"java.lang.Object".equals(clazz.getName())) {
+                Field[] fields = clazz.getDeclaredFields();
+                for (Field field : fields) {
+                    if (field.getName().equals(fieldName)) {
+                        field.setAccessible(true);
+                        // 判断类型
+                        typeName = field.getGenericType().getTypeName();
+                        switch (typeName) {
+                            case "java.lang.String":
+                                field.set(entity, fieldValue);
+                                break;
+                            case "java.lang.Integer":
+                                field.set(entity, new Integer(fieldValue));
+                                break;
+                            case "java.lang.Double":
+                                field.set(entity, new Double(fieldValue));
+                                break;
+                            case "java.math.BigDecimal":
+                                field.set(entity, new BigDecimal(fieldValue));
+                                break;
+                        }
+                        field.setAccessible(false);
+                    }
+                }
+                clazz = clazz.getSuperclass();
+            }
+        }
+    }
+
+}
