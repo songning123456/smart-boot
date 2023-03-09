2c3a2aaca347d6d8f2bef980b77ee6100ea8817e
sonin <1457065857@qq.com>
2023-03-09 10:22:42 +0800

    添加常量

---
+++ boot-module-base/src/main/java/com/sonin/core/constant/BaseConstant.java
@@ -0,0 +1,15 @@
+package com.sonin.core.constant;
+
+/**
+ * <pre>
+ * 常量
+ * </pre>
+ *
+ * @author sonin
+ * @version 1.0 2023/3/9 10:12
+ */
+public interface BaseConstant {
+
+    String dateFormat = "yyyy-MM-dd HH:mm:ss";
+
+}
