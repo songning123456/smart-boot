0282b0c7a4b9dcac406b5f9b6549c9285be7f73a
sonin <1457065857@qq.com>
2023-03-29 14:02:29 +0800

    动态数据源切换测试

--- boot-module-clean/src/test/java/com/sonin/BootApplicationTest.java
+++ boot-module-clean/src/test/java/com/sonin/BootApplicationTest.java
@@ -1,13 +1,15 @@
 package com.sonin;
 
+import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
 import com.sonin.modules.base.service.IBaseService;
-import com.sonin.modules.sequence.service.impl.SequenceService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.LinkedHashMap;
 
 /**
  * <pre>
@@ -24,13 +26,33 @@ public class BootApplicationTest {
 
     @Autowired
     private IBaseService baseService;
-    @Autowired
-    private SequenceService sequenceService;
 
+    /**
+    * <pre>
+    * 动态数据源切换测试
+    * </pre>
+     * @param
+    * @author sonin
+    * @Description: TODO(这里描述这个方法的需求变更情况)
+    */
     @Test
-    public void test() {
-        long id = sequenceService.nextId();
-        System.out.println(id);
+    public void DSTest() {
+        // 切换test数据源
+        DynamicDataSourceContextHolder.push("test");
+        baseService.insert("sys_log", new LinkedHashMap<String, Object>(){{
+            put("id", "1");
+        }});
+        DynamicDataSourceContextHolder.poll();
+        // 切换test2数据源
+        DynamicDataSourceContextHolder.push("test2");
+        baseService.insert("sys_log", new LinkedHashMap<String, Object>(){{
+            put("id", "2");
+        }});
+        DynamicDataSourceContextHolder.poll();
+        // 默认执行master数据源
+        baseService.insert("sys_log", new LinkedHashMap<String, Object>(){{
+            put("id", "3");
+        }});
     }
 
 }
