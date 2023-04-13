dff9403525368330be68db7d639d6f0cf3afa79e
sonin <1457065857@qq.com>
2023-04-13 18:00:53 +0800

    优化

--- boot-module-biz/src/test/java/com/sonin/BootApplicationTest.java
+++ boot-module-biz/src/test/java/com/sonin/BootApplicationTest.java
@@ -16,7 +16,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.support.TransactionTemplate;
