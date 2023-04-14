48b37331e074fe6904b4d961f80cff8a2c8529c8
sonin <1457065857@qq.com>
2023-04-14 10:47:23 +0800

    magic启动成功

--- boot-module-biz-magic/src/main/java/com/sonin/config/MybatisPlusConfig.java
+++ boot-module-biz-magic/src/main/java/com/sonin/config/MybatisPlusConfig.java
@@ -1,7 +1,6 @@
 package com.sonin.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
