2bb5f6ede15ae464b41f1da3b84437c9555bef9d
sonin <1457065857@qq.com>
2023-04-19 22:23:52 +0800

    定时任务insert

--- boot-module-biz-ynlj/src/main/java/com/sonin/modules/quartz/ScheduleJob.java
+++ boot-module-biz-ynlj/src/main/java/com/sonin/modules/quartz/ScheduleJob.java
@@ -33,14 +33,14 @@ public class ScheduleJob {
 
     /**
      * <pre>
-     * 实时数据：每隔1min执行一次
+     * 实时数据：每隔5s执行一次
      * </pre>
      *
      * @param
      * @author sonin
      * @Description: TODO(这里描述这个方法的需求变更情况)
      */
-    @Scheduled(cron = "0 */1 * * * ?")
+    @Scheduled(cron = "*/5 * * * * ?")
     public void realTimeDataJob() {
         try {
             String time = DateUtils.date2Str(new Date(), BaseConstant.dateFormat);
@@ -68,6 +68,27 @@ public class ScheduleJob {
             log.info("开始执行 hourDataJob:" + time);
             hourJob(1);
             log.info("结束执行 hourDataJob:" + time);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * <pre>
+     * 历史数据：每隔5s执行一次
+     * </pre>
+     *
+     * @param
+     * @author sonin
+     * @Description: TODO(这里描述这个方法的需求变更情况)
+     */
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void insertDataJob() {
+        try {
+            String time = DateUtils.date2Str(new Date(), BaseConstant.dateFormat);
+            log.info("开始执行 insertDataJob:" + time);
+            insertJob(1);
+            log.info("结束执行 insertDataJob:" + time);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -143,6 +164,55 @@ public class ScheduleJob {
                     String val = DigitalUtils.nPoint(min + (Math.random() * (max - min + 1)), 2);
                     String updateSql = "update " + tableName + " set v = '" + val + "' where id = '" + id + "'";
                     pgDB.execute(updateSql);
+                }
+            }
+        }
+    }
+
+    public void insertJob(int type) {
+        JdbcTemplate masterDB = (JdbcTemplate) SpringContext.getBean("master");
+        JdbcTemplate pgDB = (JdbcTemplate) SpringContext.getBean("pg-db");
+        String nm, metricUidTag, initTime = "1680328800";
+        if (type == 1) {
+            initTime = "" + (System.currentTimeMillis() / 1000 - 60);
+        }
+        double min = 0D, max = 0D;
+        String yearMonth = DateUtils.date2Str(new Date(), "yyyyMM");
+        // 遍历每一个厂站
+        for (String departId : departId2NameMap.keySet()) {
+            List<Map<String, Object>> queryMapList = masterDB.queryForList("select * from sys_monitor_metric_info where 1=1 and metric_uid_tag in ('GJSS', 'GJCOD', 'GJNH3N') and depart_id = '" + departId + "'");
+            for (Map<String, Object> item : queryMapList) {
+                nm = "" + item.get("id");
+                metricUidTag = "" + item.get("metric_uid_tag");
+                switch (metricUidTag) {
+                    case "GJSS":
+                        min = 23D;
+                        max = 27D;
+                        break;
+                    case "GJCOD":
+                        min = 180D;
+                        max = 190D;
+                        break;
+                    case "GJNH3N":
+                        min = 3D;
+                        max = 5D;
+                        break;
+                }
+                for (int i = 1; i <= 31; i++) {
+                    try {
+                        String suffix = (i < 10) ? ("0" + i) : ("" + i);
+                        String tableName = "xsinsert" + yearMonth + suffix;
+                        String selectSql = "select * from " + tableName + " where ts >= '" + initTime + "' and nm = '" + nm + "'";
+                        List<Map<String, Object>> mapList = pgDB.queryForList(selectSql);
+                        for (Map<String, Object> map : mapList) {
+                            String id = "" + map.get("id");
+                            String val = DigitalUtils.nPoint(min + (Math.random() * (max - min + 1)), 2);
+                            String updateSql = "update " + tableName + " set v = '" + val + "' where id = '" + id + "'";
+                            pgDB.execute(updateSql);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
