eaeaf7cdfbbe185a77512e00171e925daef37973
sonin <1457065857@qq.com>
2023-04-20 09:31:45 +0800

    biz-empty优化

--- boot-module-biz-empty/src/test/java/com/sonin/BootApplicationTest.java
+++ boot-module-biz-empty/src/test/java/com/sonin/BootApplicationTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.support.TransactionTemplate;
 
 
 /**
@@ -25,12 +24,9 @@ public class BootApplicationTest {
 
     @Autowired
     private IBaseService baseService;
-    @Autowired
-    private TransactionTemplate transactionTemplate;
 
     @Test
-    public void QuartzTest() {
-
+    public void test() {
     }
 
 }
