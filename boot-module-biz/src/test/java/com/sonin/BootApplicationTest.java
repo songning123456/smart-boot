b3156df2cb76b8a36720c811869fe8f3e8851f98
sonin <1457065857@qq.com>
2024-05-31 15:52:28 +0800

    druid连接池支持clickhouse

--- boot-module-biz/src/test/java/com/sonin/BootApplicationTest.java
+++ boot-module-biz/src/test/java/com/sonin/BootApplicationTest.java
@@ -5,6 +5,7 @@ import com.sonin.core.constant.BusinessConstant;
 import com.sonin.core.context.SpringContext;
 import com.sonin.core.entity.CaseWhen;
 import com.sonin.core.mpp.DataSourceTemplate;
+import com.sonin.modules.base.constant.BaseConstant;
 import com.sonin.modules.base.service.IBaseService;
 import com.sonin.utils.DateUtils;
 import com.sonin.utils.ConvertUtils;
@@ -18,15 +19,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.Reader;
 import java.math.BigDecimal;
 import java.util.*;
-import java.util.stream.Collectors;
-
 
 /**
  * <pre>
@@ -136,61 +134,6 @@ public class BootApplicationTest {
 
     /**
      * <pre>
-     * 模拟数据
-     * </pre>
-     *
-     * @param
-     * @author sonin
-     * @Description: TODO(这里描述这个方法的需求变更情况)
-     */
-    @Test
-    public void generateDataTest() {
-        String yearMonthDayStr = "2023-08-24";
-        List<String> nmList = new ArrayList<String>() {{
-            add("demo_test");
-        }};
-        String startTime = yearMonthDayStr + " 00:00:00";
-        String endTime = yearMonthDayStr + " 23:59:59";
-        String countTable = "ffs_count";
-        long startTs = DateUtils.strToDate(startTime, BusinessConstant.dateFormat).getTime() / 1000;
-        long endTs = DateUtils.strToDate(endTime, BusinessConstant.dateFormat).getTime() / 1000;
-        List<Map<String, Object>> historyDataList = new ArrayList<>();
-        List<Map<String, Object>> countDataList = new ArrayList<>();
-        int index = 0;
-        for (String nm : nmList) {
-            for (long ts = startTs; ts <= endTs; ts += 10) {
-                long finalTs = ts;
-                historyDataList.add(new HashMap<String, Object>() {{
-                    put("nm", nm);
-                    put("v", ConvertUtils.intRandom(0, 100));
-                    put("ts", finalTs);
-                }});
-            }
-            for (long ts = startTs; ts <= endTs; ts += 3600) {
-                String hourStr = DateUtils.sec2DateStr(ts, BusinessConstant.dateFormat.substring(0, 13)) + ":00:00";
-                Long hourTs = DateUtils.dateStr2Sec(hourStr, BusinessConstant.dateFormat);
-                int finalIndex = index;
-                countDataList.add(new HashMap<String, Object>() {{
-                    put("id", finalIndex);
-                    put("nm", nm);
-                    put("v", ConvertUtils.intRandom(0, 100));
-                    put("ts", ConvertUtils.getString(hourTs));
-                }});
-                index++;
-            }
-        }
-        if (!historyDataList.isEmpty() && !countDataList.isEmpty()) {
-            DataSourceTemplate.execute("pg-db", () -> {
-                baseService.insert("realtimedata", historyDataList.get(historyDataList.size() - 1));
-                baseService.insertBatch("xsinsert" + yearMonthDayStr.replaceAll("-", ""), historyDataList);
-                baseService.insertBatch(countTable, countDataList);
-                return 1;
-            });
-        }
-    }
-
-    /**
-     * <pre>
      * case when 测试
      * </pre>
      *
@@ -220,77 +163,12 @@ public class BootApplicationTest {
     }
 
     /**
-     * <pre>
-     * xsinert 转换成 count
-     * </pre>
-     *
-     * @param
-     * @author sonin
-     * @Description: TODO(这里描述这个方法的需求变更情况)
+     * 连接clickhouse测试
      */
     @Test
-    public void xsinsert2countTest() {
-        String startTime = "2023-12-09 00:00:00";
-        String endTime = "2023-12-14 23:59:59";
-        List<String> timeList = DateUtils.intervalByHour(startTime, endTime, BusinessConstant.dateFormat.substring(0, 14));
-        // 查询depart_id => device_id 转换关系
-        List<Map<String, Object>> deviceMapList = baseService.queryForList("select depart_id, device_id from sys_factory_device", new QueryWrapper<>());
-        Map<String, String> depart2DeviceMap = deviceMapList.stream().collect(Collectors.toMap(item -> ConvertUtils.getString(item.get("depart_id")), item -> ConvertUtils.getString(item.get("device_id")), (v1, v2) -> v2));
-        JdbcTemplate pgDB = (JdbcTemplate) SpringContext.getBean("pg-db");
-        for (String time : timeList) {
-            String tableSuffix = time.substring(0, 10).replaceAll("-", "");
-            String tmpStartTime = time + "00:00";
-            String tmpStartTs = ConvertUtils.getString(DateUtils.dateStr2Sec(tmpStartTime, BusinessConstant.dateFormat));
-            String tmpEndTime = time + "59:59";
-            String tmpEndTs = ConvertUtils.getString(DateUtils.dateStr2Sec(tmpEndTime, BusinessConstant.dateFormat));
-            Map<String, String[]> nm2InfoMap = new LinkedHashMap<>();
-            Map<String, List<String>> nm2ValListMap = new LinkedHashMap<>();
-            // 查询这一个小时的所有数据(按照时间升序)
-            List<Map<String, Object>> queryMapList = pgDB.queryForList("select nm, v, ts, factoryname, type from xsinsert" + tableSuffix + " where ts >= ? and ts <= ? order by ts asc", tmpStartTs, tmpEndTs);
-            for (Map<String, Object> item : queryMapList) {
-                String nm = ConvertUtils.getString(item.get("nm"));
-                String v = ConvertUtils.getString(item.get("v"));
-                String ts = ConvertUtils.getString(item.get("ts"));
-                String factoryName = ConvertUtils.getString(item.get("factoryname"));
-                String type = ConvertUtils.getString(item.get("type"));
-                nm2InfoMap.put(nm, new String[]{type, factoryName});
-                nm2ValListMap.putIfAbsent(nm, new ArrayList<>());
-                nm2ValListMap.get(nm).add(v);
-            }
-            // 插入数据(挨个插入nm)
-            for (Map.Entry<String, String[]> entry : nm2InfoMap.entrySet()) {
-                String nm = entry.getKey();
-                String type = entry.getValue()[0];
-                String factoryName = entry.getValue()[1];
-                String tableNamePrefix = depart2DeviceMap.get(factoryName);
-                // 不存在则直接跳过
-                if (StringUtils.isEmpty(tableNamePrefix)) {
-                    continue;
-                }
-                String tableName = tableNamePrefix.toLowerCase() + "_count";
-                List<String> valList = nm2ValListMap.get(nm);
-                Double val = null;
-                if ("3".equals(type) && valList.size() > 1) {
-                    // 求差
-                    val = ConvertUtils.getDouble(valList.get(valList.size() - 1), 0D) - ConvertUtils.getDouble(valList.get(0), 0D);
-                } else if ("4".equals(type)) {
-                    // 求平均
-                    val = valList.stream().mapToDouble(item -> ConvertUtils.getDouble(item, 0D)).average().getAsDouble();
-                }
-                if (val != null) {
-                    // 先查询此nm, ts是否有值，无值才补录
-                    Map<String, Object> countQueryMap = pgDB.queryForMap("select count(*) as total from " + tableName + " where nm = ? and ts = ?", nm, tmpStartTs);
-                    double total = ConvertUtils.getDouble(countQueryMap.get("total"), 0D);
-                    if (total == 0) {
-                        try {
-                            pgDB.update("insert into " + tableName + "(nm, v, ts, createtime, factoryname, devicename, type, gatewaycode) values(?, ?, ?, ?, ?, ?, ?, ?)", nm, val, tmpStartTs, Long.parseLong(tmpStartTs), factoryName, "custom", type, factoryName);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }
+    public void clickhouseTest() {
+        List<Map<String, Object>> queryMapList0 = DataSourceTemplate.execute("ck-ds", () -> baseService.queryForList("select * from base_customer", new QueryWrapper<>().last("limit 10")));
+        System.out.println(queryMapList0);
     }
 
     /**
@@ -310,21 +188,21 @@ public class BootApplicationTest {
             put("create_by", "sonin0");
         }};
         // 检查主键，重复会报错，res0=1
-        int res0 = baseService.insert("demo_n", entity0Map, com.sonin.modules.base.constant.BaseConstant.INSERT);
+        int res0 = baseService.insert("demo_n", entity0Map, BaseConstant.INSERT);
         // 测试insert ignore
         Map<String, Object> entity1Map = new LinkedHashMap<String, Object>() {{
             put("id", "1");
             put("create_by", "sonin1");
         }};
         // 忽略主键相同的数据，res1=0
-        int res1 = baseService.insert("demo_n", entity1Map, com.sonin.modules.base.constant.BaseConstant.INSERT_IGNORE);
+        int res1 = baseService.insert("demo_n", entity1Map, BaseConstant.INSERT_IGNORE);
         // 测试replace
         Map<String, Object> entity2Map = new LinkedHashMap<String, Object>() {{
             put("id", "1");
             put("create_by", "sonin2");
         }};
         // 若有相同主键则替换，若无则新生成，res2=2
-        int res2 = baseService.insert("demo_n", entity2Map, com.sonin.modules.base.constant.BaseConstant.REPLACE);
+        int res2 = baseService.insert("demo_n", entity2Map, BaseConstant.REPLACE);
         System.out.println("end");
     }
 
