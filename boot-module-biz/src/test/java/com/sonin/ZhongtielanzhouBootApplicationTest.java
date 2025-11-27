4446426f2a31aa15dfb142ca0b98d1373defd065
sonin <1457065857@qq.com>
2025-11-27 14:38:52 +0800

    中铁兰州初步提交

---
+++ boot-module-biz/src/test/java/com/sonin/ZhongtielanzhouBootApplicationTest.java
@@ -0,0 +1,29 @@
+package com.sonin;
+
+import com.sonin.modules.mpp.service.IMPPService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * <pre>
+ * Spring Test
+ * </pre>
+ *
+ * @author sonin
+ * @version 1.0 2022/4/25 16:30
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BootApplication.class)
+@ActiveProfiles("zhongtielanzhou")
+public class ZhongtielanzhouBootApplicationTest {
+
+    @Autowired
+    private IMPPService baseService;
+
+}
